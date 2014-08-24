7c0480da5db97d40e1770a71796abc08e14923ea
Mike Heffner <mikeh@fesnel.com>
2014-08-24 17:35:57 -0400

    Updated players.

--- parsecsv.rb
+++ parsecsv.rb
@@ -1,29 +1,32 @@
 #!/usr/bin/env ruby
 
-File.open("raw.csv").each do |line|
+File.open("rawcsv.csv").each do |line|
   line.chomp!
   csv = line.split(",")
-  if csv.length == 7
-    csv2 = [csv[0], csv[1], csv[2], "#{csv[3]} #{csv[4]}", csv[5], csv[6]]
-    csv = csv2
-  end
+  # if csv.length == 7
+  #   csv2 = [csv[0], csv[1], csv[2], "#{csv[3]} #{csv[4]}", csv[5], csv[6]]
+  #   csv = csv2
+  # end
 
   next if csv.length == 0
 
-  if csv.length != 6
+  if csv.length != 5
+    puts csv.inspect
     exit 1
   end
 
-  posre = /\(([A-Z]+)([0-9]+)\)/
-  posm = csv[1].match(posre)
+ # puts csv.inspect
+  posre = /([A-Z]+)([0-9]+)/
+  posm = csv[4].match(posre)
+#  puts "posm #{posm.inspect}"
   exit 1 unless posm
 
   out = [csv[0].to_i,
          posm[1],
          posm[2],
-         "#{csv[2]} #{csv[3]}",
-         csv[4],
-         csv[5]]
+         csv[1],
+         csv[2],
+         csv[3]]
 
   #puts "in: #{csv.inspect}"
   #puts "out: #{out.inspect}"

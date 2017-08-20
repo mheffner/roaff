fa22868a6f0d9ecd38d7b774b475bbc68c6f4a13
Mike Heffner <mike@librato.com>
2017-08-20 19:30:15 -0400

    Fix title and form box.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def signed_in?
     session[:is_signed_in] == true
   end
+
+  def app_title
+    year = Time.now.year
+
+    "Roanoke Fantasy Football #{year}"
+  end
 end

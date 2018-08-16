832e04ae729729b0edc9f821bc2d0143a49b8cdc
Mike Heffner <mike@librato.com>
2018-08-15 22:10:21 -0400

    Update for 2018

--- parse-ffpros-csv-to-csv.rb
+++ parse-ffpros-csv-to-csv.rb
@@ -26,6 +26,7 @@ csv = CSV.table(file)
 
 csv.each do |l|
   #puts "l #{l.inspect}"
+  next if l[:rank].to_s == ""
 
   m = l[:pos].match(/\A([A-Z]+)([0-9]+)\Z/) rescue nil
   unless m
@@ -36,21 +37,31 @@ csv.each do |l|
   overall = l[:rank]
   position = defense(m[1])
   posrank = m[2]
-  player = l[:player]
+  player = l[:overall]
   team = l[:team]
   bye = l[:bye]
 
   next if team == "FA"
 
+  # parse: player == Indianapolis (IND), team == ""
+  if position == "DEF"
+    m = player.match(/\A(.+) \((.+)\)\Z/) rescue nil
+    if m
+      player = m[1]
+      team = m[2]
+    end
+  end
+
+  arr = [overall,
+         position,
+         posrank,
+         player,
+         team,
+         bye]
   begin
-    print_csv([overall,
-               position,
-               posrank,
-               player,
-               team,
-               bye])
+    print_csv(arr)
   rescue => e
-    puts "unable to parse #{l.inspect}"
+    puts "unable to parse #{l.inspect} from #{arr.inspect}"
     puts "parsed as: #{e.inspect}"
     exit 1
   end
